ced74a1c779f3972b459354f8b3335fa96402c77
Adam Mulligan <adammulligan@gmail.com>
2014-04-03 14:15:49 +0100

    Precompile devise assets as they are not in a manifest

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ module MangroveValidation
     # config.autoload_paths += %W(#{config.root}/extras)
     config.autoload_paths << "#{Rails.root}/app/models/enumerations"
 
+    config.assets.precompile += [ 'devise.js', 'devise.css' ]
+
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
