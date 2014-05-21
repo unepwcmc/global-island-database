ebb505ec43a3165f84c431fe70d81f09cad1e872
Adam Mulligan <adammulligan@gmail.com>
2014-05-21 08:40:26 +0100

    Only request valid columns from cartodb geom view

--- app/models/download_job.rb
+++ app/models/download_job.rb
@@ -70,7 +70,7 @@ class DownloadJob
   end
 
   def geometry_download_url
-    cartodb_url "SELECT id_gid, the_geom, action, status FROM #{APP_CONFIG['cartodb_table']}_view"
+    cartodb_url "SELECT id_gid, the_geom FROM #{APP_CONFIG['cartodb_table']}_view"
   end
 
   def attributes_download_url
