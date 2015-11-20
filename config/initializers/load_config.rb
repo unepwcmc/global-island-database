df11372b8e0dbadba70c98c41d379b7c24c713b0
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2015-11-20 00:09:55 +0000

    use env variables in config files

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,1 +1,9 @@
-APP_CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env].merge YAML.load_file("#{Rails.root}/config/http_auth_config.yml")[Rails.env]
+APP_CONFIG = YAML.load_file(
+  "#{Rails.root}/config/config.yml"
+)[Rails.env]
+
+AUTH_CONFIG = YAML.load(
+  ERB.new(File.read("#{Rails.root}/config/http_auth_config.yml")).result
+)[Rails.env]
+
+APP_CONFIG.merge!(AUTH_CONFIG)
